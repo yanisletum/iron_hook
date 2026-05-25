55f478223be300c9d01388fd7ba83dda942d846f
Yanis <burning43arrow@gmail.com>
2026-05-25 20:19:56 +0300

    Fix CSRF verification for admin login

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ DEBUG = env('DEBUG')
 # Читаем адреса из .env и превращаем их в список
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
+CSRF_TRUSTED_ORIGINS = ['https://iron-hook-burning43arrow.amvera.io']
+
 
 # Application definition
 
