2983304646a6e5e669ca9822368e312f50655caa
Yanis <burning43arrow@gmail.com>
2026-06-01 12:20:03 +0300

    Add markdown rendering and fix article content

---
+++ blog/templatetags/markdown_extras.py
@@ -0,0 +1,9 @@
+import markdown
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter
+def render_markdown(value):
+    return mark_safe(markdown.markdown(value, extensions=['extra']))
